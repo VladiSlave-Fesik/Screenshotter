2e8978c5b3f03fd42c14c9af0009b3448354091c
KugekiSoto <99354516+KugekiSoto@users.noreply.github.com>
2022-04-23 19:50:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ while True:
         tes = pytesseract.image_to_string(Image.open('C:\\Screenshots\\t.png'), lang=clip_lang)
         pclip.copy(tes)
         os.remove(directory + 't.png')
-        print('Скопировано в буфер')
+        print('Copied to clipboard')
     if k.is_pressed(comb3):
         with Listener(on_click=on_press) as listener:
             listener.join()
